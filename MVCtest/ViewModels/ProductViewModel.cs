e9ad82f5f5069ce7ff7e8fcc10083969266359c3
benblackcake <ben.chang@aiesec.net>
2019-05-14 15:41:24 +0800

    更新圖檔 product service

---
+++ MVCtest/ViewModels/ProductViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCtest.ViewModels
+{
+    public class ProductViewModel
+    {
+        public int Product_Id { get; set; }
+        public string Product_Name { get; set; }
+        public int Category_Id { get; set; }
+        public string UnitPrice { get; set; }
+        public string Stock { get; set; }
+        public string Product_Image { get; set; }
+        public string Size { get; set; }
+
+        public double Weight { get; set; }
+
+        public int Seller_ID { get; set; }
+
+    }
+}
