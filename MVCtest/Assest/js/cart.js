a399a063140ddc5d1ef25970915c125c9c248b81
SUSU <microsoft0313@gmail.com>
2019-05-15 17:54:26 +0800

    cart01

--- MVCtest/Assest/js/cart.js
+++ MVCtest/Assest/js/cart.js
@@ -1,35 +1,47 @@
-﻿(function () {
-    window.inputNumber = function (el) {
-        var min = el.attr('min') || false;
-        var max = el.attr('max') || false;
+﻿window.onload = function () {
+    var count = document.getElementById("count");
+    var inc = document.getElementById("inc");
+    var dec = document.getElementById("dec");
+    inc.onclick = function () {
+        count.innerHTML = parseInt(count.value) - 1;
+    };
+    dec.onclick = function () {
+        count.innerHTML = parseInt(count.value) + 1;
+    };
+}
 
-        var els = {};
-        els.dec = el.prev();
-        els.inc = el.next();
-        el.each(function () {
-            init($(this));
-        });
-        function init(el) {
-            els.dec.on('click', decrement);
-            els.inc.on('click', increment);
-            function decrement() {
-                var value = el[0].value;
-                value--;
-                if (!min || value >= min) {
-                    el[0].value = value;
-                }
-            }
-            function increment() {
-                var value = el[0].value;
-                value++;
-                if (!max || value <= max) {
-                    el[0].value = value++;
-                }
-            }
-        }
-    }
-    inputNumber($('.input-number'));
-})();
+//(function () {
+//    window.inputNumber = function (el) {
+//        var min = el.attr('min') || false;
+//        var max = el.attr('max') || false;
+
+//        var els = {};
+//        els.dec = el.prev();
+//        els.inc = el.next();
+//        el.each(function () {
+//            init($(this));
+//        });
+//        function init(el) {
+//            els.dec.on('click', decrement);
+//            els.inc.on('click', increment);
+//            function decrement() {
+//                var value = el[0].value;
+//                value--;
+//                if (!min || value >= min) {
+//                    el[0].value = value;
+//                }
+//            }
+//            function increment() {
+//                var value = el[0].value;
+//                value++;
+//                if (!max || value <= max) {
+//                    el[0].value = value++;
+//                }
+//            }
+//        }
+//    }
+//    inputNumber($('.input-number'));
+//})();
 
 
 //$(function () {
