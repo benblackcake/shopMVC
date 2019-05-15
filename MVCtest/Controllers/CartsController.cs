3b37c9833bd1e5a7ce0431bebd6e64dc19177df9
NereidYan <jun860120@gmail.com>
2019-05-15 15:26:00 +0800

    使用CRUD抓資料、button目前沒辦法更新數量，嘗試計算總價格

---
+++ MVCtest/Controllers/CartsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVCtest.Models;
+
+namespace MVCtest.Controllers
+{
+    public class CartsController : Controller
+    {
+        private DBModel db = new DBModel();
+
+        // GET: Carts
+        public ActionResult Index()
+        {
+            var carts = db.Carts.Include(c => c.Customer).Include(c => c.Product);
+            return View(carts.ToList());
+        }
+
+        // GET: Carts/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cart cart = db.Carts.Find(id);
+            if (cart == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cart);
+        }
+
+        // GET: Carts/Create
+        public ActionResult Create()
+        {
+            ViewBag.Customer_ID = new SelectList(db.Customers, "Customer_ID", "Customer_Name");
+            ViewBag.Product_ID = new SelectList(db.Products, "Product_Id", "Product_Name");
+            return View();
+        }
+
+        // POST: Carts/Create
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Cart_ID,Product_ID,Customer_ID,Quantity")] Cart cart)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Carts.Add(cart);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Customer_ID = new SelectList(db.Customers, "Customer_ID", "Customer_Name", cart.Customer_ID);
+            ViewBag.Product_ID = new SelectList(db.Products, "Product_Id", "Product_Name", cart.Product_ID);
+            return View(cart);
+        }
+
+        // GET: Carts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cart cart = db.Carts.Find(id);
+            if (cart == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Customer_ID = new SelectList(db.Customers, "Customer_ID", "Customer_Name", cart.Customer_ID);
+            ViewBag.Product_ID = new SelectList(db.Products, "Product_Id", "Product_Name", cart.Product_ID);
+            return View(cart);
+        }
+
+        // POST: Carts/Edit/5
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Cart_ID,Product_ID,Customer_ID,Quantity")] Cart cart)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cart).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Customer_ID = new SelectList(db.Customers, "Customer_ID", "Customer_Name", cart.Customer_ID);
+            ViewBag.Product_ID = new SelectList(db.Products, "Product_Id", "Product_Name", cart.Product_ID);
+            return View(cart);
+        }
+
+        // GET: Carts/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cart cart = db.Carts.Find(id);
+            if (cart == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cart);
+        }
+
+        // POST: Carts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Cart cart = db.Carts.Find(id);
+            db.Carts.Remove(cart);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
