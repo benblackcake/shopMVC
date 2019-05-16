c27c38fb77b0afcf31aedf6afc8853c6a732f2ae
StevenChengEx <frf2isgood@gmail.com>
2019-05-16 18:45:18 +0800

    add right nav

--- MVCtest/Assest/js/NavJS.js
+++ MVCtest/Assest/js/NavJS.js
@@ -1,5 +1,36 @@
 ﻿$(function () {
-    var s = "";
-    s = document.body.clientWidth
-    console.info(s);
+    $(".icon").click(function () {
+        var x = document.getElementById("testnav");
+        if (x.className === "top-menu") {
+            x.className += " responsive";
+            $(".idk").addClass("responsive");
+            $(".sidenav ").addClass("responsive");
+            document.getElementById("mySidenav").style.width = "250px";
+            document.getElementById("main").style.marginLeft = "250px";
+            document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+       } // else {
+        //    x.className = "top-menu";
+        //    $(".idk").removeClass("responsive");
+        //    $(".sidenav ").removeClass("responsive");
+        //    document.getElementById("mySidenav").style.width = "0";
+        //    document.getElementById("main").style.marginLeft = "0";
+        //    document.body.style.backgroundColor = "white";
+            
+        //}
+       
+    })
+   
+    $("#mySidenav").click(function () {
+        var x = document.getElementById("testnav");
+        x.className = "top-menu";
+        $(".idk").removeClass("responsive");
+        $(".sidenav ").removeClass("responsive");
+        document.getElementById("mySidenav").style.width = "0";
+        document.getElementById("main").style.marginLeft = "0";
+        document.body.style.backgroundColor = "white";
+    })
+    function closeNav() {
+        
+    }
+        
 })
