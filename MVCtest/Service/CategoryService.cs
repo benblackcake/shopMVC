bf1d9e390de2faf952819d79961aa15c3a21696b
daniel <qazwsx88520@gmail.com>
2019-05-20 04:09:13 +0800

    hover標題出現的列表與資料庫連結 還未完成git add .

---
+++ MVCtest/Service/CategoryService.cs
@@ -0,0 +1,30 @@
+﻿using MVCtest.Models;
+using MVCtest.Repository;
+using MVCtest.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCtest.Service
+{
+    public class CategoryService
+    {
+        public void GetCate()
+        {
+            DBModel context = new DBModel();
+            DbRepository<CategoryGroup> Crepo = new DbRepository<CategoryGroup>(context);
+            DbRepository<Sub_Categroy> Srepo = new DbRepository<Sub_Categroy>(context);
+            
+            var result =
+                from c in Crepo.GetAll()
+                join s in Srepo.GetAll()
+                on c.Category_Id equals s.Category_ID
+                where c.Category_Name == "衣服"
+                select
+                new CateViewModel
+                { Category_Id = c.Category_Id, Category_Name = s.Category_Name };
+            
+        }
+    }
+}
---
+++ MVCtest/ViewModels/CateViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCtest.ViewModels
+{
+    public class CateViewModel
+    {
+        public int Category_Id { get; set; }
+
+        public string Category_Name { get; set; }
+
+        public int? Sub_Category_ID { get; set; }
+    }
+}
