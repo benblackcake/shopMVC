bf395ea91057af484dd5504f565520c3c22380a9
Zhou <loveuu860426@gmail.com>
2019-05-22 12:22:55 +0800

    重寫productFilter.js 修正RWD錯誤 新增篩選部分的css

--- MVCtest/Assest/js/productFilter.js
+++ MVCtest/Assest/js/productFilter.js
@@ -1,30 +1,85 @@
 ﻿var $filter = $('.filter');
-$filter.on('change', function (e) {
-    e.preventDefault();
-    var filterList = [];
+var $categoryFilter = $('.category-filter');
+var $colorFilter = $('.color-filter');
+var $sizeFilter = $('.size-filter');
+var $productItem = $('.productItem'); 
+var filterList = []
 
+$categoryFilter.on('change', function () {
+    fnFilter('category');
+});
+$colorFilter.on('change', function () {
+    fnFilter('color');
+});
+$sizeFilter.on('change', function () {
+    fnFilter('size');
+});
+
+
+function fnFilter(condition) {
+
+    $($productItem).removeClass('show show-' + condition);
+
+
+
+    filterList = [];
     for (var i = 0; i < $filter.length; i++) {
-
         var $this = $($filter[i]);
-        var $dataFilter = $($this).attr('data-filter');
+        var $dataFilter = $($this).attr('data-' + condition);
         if ($this.prop('checked')) {
-
             filterList.push($dataFilter);
-
         }
     }
-    if (filterList.length == 0) {
-        $('.product-item').fadeIn();
+    filterList.forEach(function (element) {
+        for (var i = 0; i < $productItem.length; i++) {
+            alert('a');
+ //           var isAnyExist = ($($productItem[i]).hasClass('show-category') || $($productItem[i]).hasClass('show-color') || $($productItem[i]).hasClass('show-size'));
+            if ($($productItem[i]).attr('data-' + condition) == element && ($($productItem[i]).hasClass('show-category') || $($productItem[i]).hasClass('show-size'))) {
+                $($productItem[i]).addClass('show show-' +condition);
+            }
+        }
+    }) 
 
+    if ($($productItem).hasClass('show')) {
+        if ($($productItem).hasClass('show') == false && isAnyExist) {
+            $('.show-category').addClass('show');
+            $('.show-color').addClass('show');
+            $('.show-size').addClass('show');
+            $($productItem).hide();
+            $('.show').fadeIn();
+            return;
+        }
     }
-    else {
-        $('.product-item').hide();
-        filterList.forEach(function (element) {
-            $("." + element).fadeIn();
-        })
 
+    $($productItem).hide();
+    $('.show').fadeIn();
 
+    if (filterList.length != 0) {
+        return;
     }
+    $($productItem).fadeIn();
+}
 
-})
 
+
+
+//$(".price").on('change', function () {
+//    var $minPrice = 0;
+//    var $maxPrice = 0;
+//    var $price1 = $("#price1").val();
+//    var $price2 = $('#price2').val();
+
+//    if ($price1 == 0 || $price2 == 0) {
+//        return;
+//    }
+//    if ($price1 >= $price2) {
+//        $maxPrice = +$price1;
+//        $minPrice = +$price2;
+//    }
+//    else {
+//        $maxPrice = +$price2;
+//        $minPrice = +$price1;
+//    }
+
+
+//});
