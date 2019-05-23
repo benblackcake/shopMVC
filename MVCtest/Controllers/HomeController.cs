86d7b10c7878f9a6c565c882a4d75057df35b744
daniel <qazwsx88520@gmail.com>
2019-05-23 11:35:51 +0800

    修改更新

--- MVCtest/Controllers/HomeController.cs
+++ MVCtest/Controllers/HomeController.cs
@@ -35,29 +35,35 @@ namespace MVCtest.Controllers
         [HttpPost]
         public ActionResult Updatemember(Customer input)
         {
-            CustomerViewModel cvm = new CustomerViewModel();
-            cvm.Customer_Email = input.Customer_Email;
-            cvm.Customer_Name = input.Customer_Name;
-            string pwd = Helper.EncodePassword(input.User_Password);
-            cvm.User_Password = pwd;
-            cvm.Customer_Phone = input.Customer_Phone;
+            //CustomerViewModel cvm = new CustomerViewModel();
+            //cvm.Customer_Email = input.Customer_Email;
+            //cvm.Customer_Name = input.Customer_Name;
+            //string pwd = Helper.EncodePassword(input.User_Password);
+            //cvm.User_Password = pwd;
+            //cvm.Customer_Phone = input.Customer_Phone;
             CustomerService service = new CustomerService();
-            Debug.Print(cvm.Customer_Email);
-            Debug.Print(cvm.Customer_Name);
-            Debug.Print(cvm.User_Password);
-            Debug.Print(cvm.Customer_Phone);
+            //Debug.Print(cvm.Customer_Email);
+            //Debug.Print(cvm.Customer_Name);
+            //Debug.Print(cvm.User_Password);
+            //Debug.Print(cvm.Customer_Phone);
 
             input.User_Password = Helper.EncodePassword(input.User_Password);
-
+            string name = input.Customer_Name;
+            string email = input.Customer_Email;
+            int id = input.Customer_ID;
             if (service.Update(input))
             {
                 TempData["message"] = "修改成功";
-                return RedirectToAction("index", "MemberCenter");
+                Session["auth"] = true;
+                Session["Name"] = name;
+                Session["Email"] = email;
+                Session["ID"] = id;
+                return RedirectToAction("memberlist", "Home");
             }
             else
             {
                 TempData["message"] = "修改失敗";
-                return RedirectToAction("index", "MemberCenter");
+                return RedirectToAction("memberlist", "Home");
             }
         }
     }
