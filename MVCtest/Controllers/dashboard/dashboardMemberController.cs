4ae79d23d285dcff4eac395904f44c2d39175fb7
benblackcake <ben.chang@aiesec.net>
2019-05-27 19:14:27 +0800

    Update New Dashoard Backend

---
+++ MVCtest/Controllers/dashboard/dashboardMemberController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCtest.Controllers.dashboard
+{
+    public class dashboardMemberController : Controller
+    {
+        // GET: dashboardMember
+        public JsonResult GetChart()
+        {
+            var data = new { Name = "kevin", Age = 40 };
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+        // GET: dashboardMember 
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
