ce0c014dbc688560882322823d50cd6e8688620f
daniel <qazwsx88520@gmail.com>
2019-05-30 23:00:14 +0800

    dashboard顧客 顯示不出來 資料撈的到

--- MVCtest/Controllers/dashboard/dashboardCustomerController.cs
+++ MVCtest/Controllers/dashboard/dashboardCustomerController.cs
@@ -10,27 +10,14 @@ namespace MVCtest.Controllers.dashboard
 {
     public class dashboardCustomerController : Controller
     {
-
-        public JsonResult GetChart()
+        [Route("api/dashboardCustomer/GetCustomer")]
+        public JsonResult GetCustomer()
         {
-            List<int> id = new List<int>();
-            List<string> email = new List<string>();
-            List<string> name = new List<string>();
-            List<string> phone = new List<string>();
             CustomerService os = new CustomerService();
             CustomerListViewModel result = os.GetAllCustomer();
-            foreach (var item in result.Items)
-            {
-                id.Add(item.Customer_ID);
-                email.Add(item.Customer_Email);
-                name.Add(item.Customer_Name);
-                phone.Add(item.Customer_Phone);
-            }
 
-
-            var datas = new { id, email,name,phone };
-
-            return Json(result, JsonRequestBehavior.AllowGet);
+            var datas = new { result.Items };
+            return Json(datas, JsonRequestBehavior.AllowGet);
         }
         // GET: dashboardCustomer
         public ActionResult Index()
