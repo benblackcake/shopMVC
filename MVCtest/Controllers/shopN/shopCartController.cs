57223ae861a3cc8b41c2c20a0c69ba7d067e291c
benblackcake <ben.chang@aiesec.net>
2019-05-31 22:41:02 +0800

    updating 產品頁面 & 產品category查詢

---
+++ MVCtest/Controllers/shopN/shopCartController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCtest.Controllers.shopN
+{
+    public class shopCartController : Controller
+    {
+        // GET: shopCart
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
