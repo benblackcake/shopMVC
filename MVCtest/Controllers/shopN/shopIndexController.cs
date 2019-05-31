9a2097cd039e9e616f8fa3f25ae91a105ef031c1
benblackcake <ben.chang@aiesec.net>
2019-05-31 14:37:36 +0800

    update 新版本前端

---
+++ MVCtest/Controllers/shopN/shopIndexController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCtest.Controllers.shopN
+{
+    public class shopIndexController : Controller
+    {
+        // GET: shopIndex
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
