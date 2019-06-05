9829da0c1c7faae868a4957191c532e8fac029fb
benblackcake <ben.chang@aiesec.net>
2019-06-05 14:33:14 +0800

    update js for bar chart

---
+++ MVCtest/ViewModels/CustomerDateViewModelcs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCtest.ViewModels
+{
+    public class CustomerDateViewModelcs
+    {
+        public int Month { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
