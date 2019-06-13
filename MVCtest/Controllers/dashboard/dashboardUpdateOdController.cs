5a43d86756f5b77c7c8ad5c6ae77cb90672ab2ae
Zhou <loveuu860426@gmail.com>
2019-06-13 22:27:15 +0800

    後台訂單明細查詢修改完成

---
+++ MVCtest/Controllers/dashboard/dashboardUpdateOdController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCtest.Models;
+using MVCtest.ViewModels;
+
+namespace MVCtest.Controllers.dashboard
+{
+    public class dashboardUpdateOdController : Controller
+    {
+        // GET: dashboardUpdateOd
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string orderId)
+        {
+            DBModel db = new DBModel();
+            List<OrderDetailViewModel> result = new List<OrderDetailViewModel>();
+            List<int> result1 = new List<int>();
+            var value =
+                from o in db.OrderDetail.ToList()
+                join pd in db.Product_Detail.ToList()
+                on o.Product_Detail_Id equals pd.Product_Detail_Id
+                where o.Order_Id == int.Parse(orderId)
+                select
+                new OrderDetailViewModel
+                {
+                    orderId = o.OrderDetail_Id,
+                    productName = o.Product_Name,
+                    unitPrice = o.UnitPrice,
+                    quantity = o.Quantity,
+                    productId = o.Product_Id,
+                    productDetailList = db.Product_Detail.Where(x=>x.Product_Id ==o.Product_Id).ToList(),
+                    pdId = o.Product_Detail_Id
+                    
+                    //size = pd.Size,
+                    //color = pd.Color
+                };
+
+            result.AddRange(value.ToList());
+            return View(result);
+        }
+        [HttpPost]
+        public ActionResult UpdateOrderDrtail(string odId,string pdId, string quantity)
+        {
+            DBModel db = new DBModel();
+            var OrderDetail_Id = int.Parse(odId);
+            var value = db.OrderDetail.Where(x => x.OrderDetail_Id == OrderDetail_Id).FirstOrDefault();
+
+            value.Product_Detail_Id = int.Parse(pdId);
+            value.Quantity = int.Parse(quantity);
+            db.SaveChanges();
+            return RedirectToAction("Index","dashboardUpdateOd");
+        }
+
+        public ActionResult DeleteOrderDetail(int odId)
+        {
+            DBModel db = new DBModel();
+            var reuslt =  db.OrderDetail.Where(x => x.OrderDetail_Id == odId).FirstOrDefault();
+            db.OrderDetail.Remove(reuslt);
+            db.SaveChanges();
+            return RedirectToAction("Index", "dashboardUpdateOd");
+        }
+    }
+}
--- MVCtest/ViewModels/OrderDetailViewModel.cs
+++ MVCtest/ViewModels/OrderDetailViewModel.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
+using MVCtest.Models;
 namespace MVCtest.ViewModels
 {
     public class OrderDetailViewModel
     {
         public int orderId { get; set; }
         public string productName { get; set; }
+        public int? productId { get; set; }
         public int? unitPrice { get; set; }
         public int? quantity { get; set; }
         public string size { get; set; }
         public string color { get; set; }
+        public int? pdId { get; set; }
+        public List<Product_Detail> productDetailList { get; set; }
     }
 }
